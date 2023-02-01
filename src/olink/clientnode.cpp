402357a5b4dcdc2ed7c2de51df4cb217576f2a0e
Wolfgang Bremer <wbremer@apigear.io>
2023-02-01 07:20:37 +0100

    fix: Correct init order in ClientNode

--- src/olink/clientnode.cpp
+++ src/olink/clientnode.cpp
@@ -8,8 +8,8 @@ namespace ApiGear { namespace ObjectLink {
 
 ClientNode::ClientNode(ClientRegistry& registry)
     : BaseNode()
-    , m_nextRequestId(0)
     , m_registry(registry)
+    , m_nextRequestId(0)
 {
 }
 
