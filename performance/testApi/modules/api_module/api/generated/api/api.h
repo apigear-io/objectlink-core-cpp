03201238af8e09ea766d1fe31f2a677f60fe888b
dphan10 <dorota.phan@siili.com>
2024-02-01 09:38:55 +0100

    feat: move performance test out repo
    they can be found now in github.com/apigear-io/performance-checks

--- performance/testApi/modules/api_module/api/generated/api/api.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "api/generated/api/datastructs.api.h"
-#include "api/generated/api/testapi.api.h"
--- performance/testApi/modules/api_module/api/generated/core/api.json.adapter.cpp
+++
@@ -1,6 +0,0 @@
-#include "api/generated/core/api.json.adapter.h"
-
-namespace Test {
-namespace Api {
-} // namespace Api
-} // namespace Test
